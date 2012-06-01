a8fbd9a061e899dd8e189de4876e695dd071058d
Mike <mikldt@gmail.com>
2012-05-31 23:07:14 -0400

    Add a working route and add ruby 1.8.7 compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# Note: currently, this goes directly into the app, so the
+# route from root is just /players. Eventually, we may want
+# to create the route against the engine, and then just mount
+# the engine from the main app. Or, we could put it in a namespace.
+
 Rails.application.routes.draw do
-  resources :players
+  scope :module => "ConcertoHardware" do
+    resources :players
+   end
 end

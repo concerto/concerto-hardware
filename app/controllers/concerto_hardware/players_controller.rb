be40af57a3ae0f88de1e5f31ef013ee59726fe0c
Mike <mikldt@gmail.com>
2013-04-17 21:03:31 -0400

    Insert missing colons, fixing RESTful JSON access.

--- app/controllers/concerto_hardware/players_controller.rb
+++ app/controllers/concerto_hardware/players_controller.rb
@@ -13,7 +13,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json => @players }
+      format.json { render :json => @players }
     end
   end
 
@@ -25,7 +25,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json => @player }
+      format.json { render :json => @player }
     end
   end
 
@@ -37,7 +37,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json => @player }
+      format.json { render :json => @player }
     end
   end
 
@@ -55,11 +55,11 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to [hardware, @player], notice => 'Player was successfully created.' }
-        format.json { render json => @player, status => :created, location => @player }
+        format.html { redirect_to [hardware, @player], :notice => 'Player was successfully created.' }
+        format.json { render :json => @player, :status => :created, :location => @player }
       else
-        format.html { render action => "new" }
-        format.json { render json => @player.errors, status => :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render :json => @player.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -72,11 +72,11 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.update_attributes(params[:player])
-        format.html { redirect_to [hardware, @player], notice => 'Player was successfully updated.' }
+        format.html { redirect_to [hardware, @player], :notice => 'Player was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action => "edit" }
-        format.json { render json => @player.errors, status => :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { render :json => @player.errors, :status => :unprocessable_entity }
       end
     end
   end

9a7191d3f2268ed3cb7cdf19c3160ea8db679fa3
Brian R Zaik <brian.r.zaik@gmail.com>
2013-12-21 15:27:42 -0500

    Fixed a syntax error - :string is not required for remove_column migration commands

--- db/migrate/20131127201048_add_updates_to_concerto_hardware_players.rb
+++ db/migrate/20131127201048_add_updates_to_concerto_hardware_players.rb
@@ -1,6 +1,6 @@
 class AddUpdatesToConcertoHardwarePlayers < ActiveRecord::Migration
   def change
-    remove_column :concerto_hardware_players, :secret, :string
+    remove_column :concerto_hardware_players, :secret
     add_column :concerto_hardware_players, :screen_on_off, :string
   end
 end
